47c02bc92a4d116cac6f9a4c353ace54bff7327a
vinnidias <vinicius.3991@gmail.com>
2020-10-17 19:16:45 -0300

    corrigindo variáveis

--- api-tests/api-post-method.test.js
+++ api-tests/api-post-method.test.js
@@ -1,10 +1,10 @@
 const postMethod = require('./api-post-method')
 
 const username = 'tomsmith'
-const password = 'SuperSecret'
+const password = 'SuperSecretPassword!'
 
 test('should return status 200 when the declared username and password are passed', done => {
-    postMethod(username, senhaErrada)
+    postMethod(username, password)
         .then(result => {
             expect(result.status).toBe(200)
             done()
