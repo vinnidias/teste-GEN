1c5249a913a3d8b421670fd3cc0f385a7d9057b9
vinnidias <vinicius.3991@gmail.com>
2020-10-18 14:12:55 -0300

    finalizando interações do bot

--- front-end-tests/front-end-test.js
+++ front-end-tests/front-end-test.js
@@ -1,15 +1,12 @@
 const puppeteer = require('puppeteer')
 
 const getInfo = async () => {
-    const browser = await puppeteer.launch({
-        headless: false
-    })
+    const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
     await page.goto('https://the-internet.herokuapp.com/login')
 
-    await page.waitFor('input[name="username"]')
-
     //Require entries
+    await page.waitFor('input[name="username"]')
     
     await page.type('input[name="username"]', 'tomsmith', {delay: 100})
     await page.type('input[name="password"]', 'SuperSecretPassword!', {delay: 100})
@@ -22,7 +19,43 @@ const getInfo = async () => {
 
     await page.waitFor('.example > a')
     await page.screenshot({path: 'secure-area-screenshot.png'})
+
+    //chromium is overlapping the component and the bot clicks the fork
+
+    await page.waitFor('#flash-messages > #flash > a')
+    await page.click('#flash-messages > #flash > a')
+    await page.screenshot({path: 'wrong-bot-click.png'})
+
+    await page.goBack()
+
     await page.click('.example > a')
+
+    //The same overlap problem 
+
+
+    await page.waitFor('#flash-messages > #flash > a')
+    await page.click('#flash-messages > #flash > a')
+
+    await page.goBack()
+
+    //invalid username message 
+    await page.waitFor('.radius')
+    await page.click('.radius')
+
+    await page.waitFor('#flash > a')
+
+    await page.screenshot({path: 'ivalid-username-message.png'})
+
+    //The same overlap problem
+    await page.click('#flash > a')
+    await page.goBack()
+
+    // navigation for github
+    await page.waitFor('.row > a')
+    await page.click('.row > a')
+    await page.screenshot({path: 'github-navigation.png'})
+
+    await page.goBack()
 
     await browser.close()
     
