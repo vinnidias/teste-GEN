b71df8ebea9e95ba3532a13ff5da400598b71662
vinnidias <vinicius.3991@gmail.com>
2020-10-18 14:14:00 -0300

    escopo teste para entrada com dados invalidos

--- api-tests/api-post-method.test.js
+++ api-tests/api-post-method.test.js
@@ -3,7 +3,15 @@ const postMethod = require('./api-post-method')
 const username = 'tomsmith'
 const password = 'SuperSecretPassword!'
 
+const wrongPassword = '1234'
+
 test('should return status 200 when the declared username and password are passed', async () => {
     const response = await postMethod(username, password)
     expect(response.status).toBe(200)
 });
+
+test('should return status 401 when the declared username and wrong password are passed', async () => {
+    const response = await postMethod(username, wrongPassword)
+    expect(response.status).toBe(401)
+});
+
