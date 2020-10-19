e732bb23ae4bed0844c0d0e23d6799a7415ac0c6
vinnidias <vinicius.3991@gmail.com>
2020-10-19 19:30:59 -0300

    executando mais dois testes para teste do cenário ruim com api

--- api-tests/api-post-method.test.js
+++ api-tests/api-post-method.test.js
@@ -3,6 +3,7 @@ const postMethod = require('./api-post-method')
 const username = 'tomsmith'
 const password = 'SuperSecretPassword!'
 const wrongPassword = '1234'
+const wrongName = 'wrongName'
 
 test('should return status 200 when the declared username and password are passed', async () => {
     const response = await postMethod(username, password)
@@ -14,3 +15,12 @@ test('should return status 401 when the declared username and wrong password are
     expect(response.status).toBe(401)
 });
 
+test('should return status 401 when the wrong username and correct password are passed', async () => {
+    const response = await postMethod(wrongName, password)
+    expect(response.status).toBe(401)
+});
+
+test('should return status 401 when the wrong username and wrong password are passed', async () => {
+    const response = await postMethod(wrongName, wrongPassword)
+    expect(response.status).toBe(401)
+});
